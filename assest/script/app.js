2b023142818d900ee1e3d424999260fc9eb014e6
MALWICK <batahumphrey66@gamil.com>
2023-06-21 11:19:30 +0100

    create Knapsack

--- assest/script/app.js
+++ assest/script/app.js
@@ -1,134 +1,110 @@
-//here declaring a variable that we ill inputt our max-wieght
-let maxWeight = document.querySelector(".weight-input");
-//here it serves to link our list
-let selectDropdown = document.querySelector(".input-list");
-// i am linking my selected item to my display
-let selectedItems = document.querySelector(".selected-display");
-//here linking my status of my knapsack
-let statusBtn = document.querySelector(".status");
-//here i am linking my submit button
-let submitBtn = document.querySelector(".submit");
-//here i am linking the div that will show my max-wieghtt that was inputed in input section
-let capacity = document.querySelector(".max-weight");
-//here i am linking my reset button
+const form = document.querySelector("form");
+const itemList = document.querySelector("#item-list");
+const totalWeight = document.querySelector("#total-weight");
+const totalValue = document.querySelector("#total-value");
+const maxWeight = document.querySelector("#max-weight");
 let clearAllBtn = document.querySelector(".clear-all");
+let Error = document.querySelector(".error");
+let Add = document.querySelector("#add");
 
-let erroCount = 0;
- 
-// it is with aid of this that i am able to use values as refrences.
-const jsonArray = [
-  { name: "fish", weight: 4, value: 1500 },
-  { name: "meat", weight: 4, value: 1500 },
-  { name: "oranges", weight: 2, value: 200 },
-  { name: "apples", weight: 2, value: 200 },
-  { name: "tomatoes", weight: 2, value: 150 },
-  { name: "rice", weight: 3, value: 700 },
-  { name: "yams", weight: 5, value: 1500 },
-  { name: "groundnut", weight: 1, value: 500 },
-  { name: "oil", weight: 3, value: 1800 },
-  { name: "mangoes", weight: 2, value: 200 },
-  { name: "bread", weight: 7, value: 100 },
-];
+let items = [];
+let maxVal = 100; // set the maximum value here // set the maximum value here
+form.addEventListener("submit", addItem);
 
-// creating my object
-let knapsack = {
-  capacity: 0,
-  weight: 0,
-  value: 0,
-  items: [],
-};
+function addItem(e) {
+  e.preventDefault();
 
-console.log(`this yo knapsack before: ${knapsack}`);
+  const itemInput = document.querySelector("#item");
+  const weightInput = document.querySelector("#weight");
+  const valueInput = document.querySelector("#value");
+  const MaximumWeight = document.querySelector("#maxi-weight");
 
-selectDropdown.addEventListener("change", () => {
-  console.log(knapsack);
+  const item = {
+    name: itemInput.value,
+    weight: parseInt(weightInput.value),
+    value: parseInt(valueInput.value),
+    maxw: parseInt(MaximumWeight.value),
+  };
 
-  maxWeight.style.border = "none";
-  let OptVal = selectDropdown.value;
-  knapsack.capacity = maxWeight.value;
-  capacity.innerHTML = `max weight: ${knapsack.capacity}kg`; // this assigns the iput capacity value to the capacity div
-
-  if (maxWeight.value == "" || maxWeight.value < 0) {
-    if (erroCount > 0) {
-      maxWeight.style.border = "2px solid red";
-      alert("your knapsack input field must have a max weight");
-    } else {
-      alert(`input a max-weight`);
-    }
-    erroCount++;
-  } else {
-    maxWeight.readonly = true;
-    weightBtn = maxWeight.value;
-    console.log(`max weight = ${maxWeight.value}kg`);
-
-    console.log(`option val = ${OptVal}`);
-
-    knapsack.weight += jsonArray[OptVal].weight;
-    console.log(`knapSack current weight = ${knapsack.weight}kg`);
-
-    if (knapsack.weight > knapsack.capacity) {
-      knapsack.weight -= jsonArray[OptVal].weight;
-      alert("this item weight will exceed knapsack capacity");
-      alert(`knapsack space left = ${knapsack.capacity - knapsack.weight}kg`);
-      selectDropdown.readonly = true;
-    } else {
-      if (knapsack.weight == knapsack.capacity) {
-        statusBtn.style.border = "2px solid blue";
-      }
-      if (knapsack.weight < knapsack.capacity) {
-        statusBtn.style.border = "2px solid green";
-      }
-
-      knapsack.items.push(
-        `[ ${jsonArray[OptVal].name} |${jsonArray[OptVal].weight}kg |${jsonArray[OptVal].value}xaf ] <br/>`
-      ); 
-      knapsack.items.join("2");
-      console.log(`knapSack items = ${knapsack.items}`);
-
-      console.log(`item weight ${jsonArray[OptVal].weight}`);
-      selectedItems.innerHTML = `${knapsack.items} <br/>`;
-      statusBtn.innerHTML = `current weight: ${knapsack.weight}Kg`;
-    }
+  if (item.weight > item.maxw) {
+    alert(`The maximum weight allowed is ${item.maxw}`);
+    console.log(`The maximum weight allowed is ${item.maxw} and its full`);
+    Add.style.backgroundColor = "red";
+    Add.disabled = true;
+    return;
   }
-});
 
-clearAllBtn.onclick = function reset () {
-  window.location.reload()
+  console.log(item.maxw, item.weight);
+  items.push(item);
+
+  displayItems();
+
+  itemInput.value = "";
+  weightInput.value = "";
+  valueInput.value = "";
+  MaximumWeight.disabled = true;
 }
 
-backBtn.addEventListener("click", () => {
- 
-  let sumWeight = 0;
-  let OptVal = selectDropdown.value;
+function displayItems() {
+  itemList.innerHTML = "";
 
-  for (let i = 0; i < jsonArray.length; i++) {
-    if (knapsack.items[i] == jsonArray[i].name) {
-      console.log(knapsack);
-      console.log(`sum: this is: '${knapsack.items[i]}' ${knapsack.items[i]}`);
-      sumWeight += jsonArray[i].weight;
+  items.forEach((item, index) => {
+    if (item.weight < item.maxw) {
+      const li = document.createElement("li");
+      li.innerHTML = `  <span>${item.name}</span>
+      <span>${item.weight}Kg</span>
+      <span>${item.value}XFA</span>
+      <button class="delete" onclick="removeItem(${index})">Remove</button>`;
+      itemList.appendChild(li);
+    } else {
+      alert(`The maximum weight allowed is ${item.maxw}`);
+      console.log(`The maximum weight allowed is ${item.maxw} and its full`);
+      Add.style.backgroundColor = "red";
+      Add.disabled = true;
+      return;
     }
+  });
+
+  calculateResult();
+}
+
+function removeItem(index) {
+  items.splice(index, 1);
+  displayItems();
+}
+
+function calculateResult() {
+  let totalWeightValue = 0;
+  let totalValueValue = 0;
+  let maxWeigh = 0;
+
+  items.forEach((item) => {
+    totalWeightValue += item.weight;
+    totalValueValue += item.value;
+    /*  maxWeight += item.maxw; */
+    maxWeigh = item.maxw;
+  });
+
+  if (totalWeightValue > maxWeigh) {
+    alert(`The maximum weight allowed is ${maxWeigh}`);
+    console.log(`The maximum weight allowed is ${maxWeigh} and its full`);
+    Add.style.backgroundColor = "red";
+    Add.disabled = true;
+    return;
+  } else {
+    totalWeight.textContent = totalWeightValue;
+    totalValue.textContent = totalValueValue;
+    maxWeight.textContent = maxWeigh;
+    return;
   }
 
-  selectedItems.innerHTML = knapsack.items;
+  /* maxWeight.textContent = maxWeight; */
+}
 
-  console.log(`knapsack before pop: ${knapsack}`);
-
-  knapsack.items.pop();
-
-  console.log(`knapsack afterpop: ${knapsack}`);
-  selectedItems.innerHTML = `${knapsack} <br/>`;
-
-  if (statusBtn.innerHTML != 0 || statusBtn.innerHTML > 0) {
-    knapsack.weight -= jsonArray[OptVal].weight;
-  }
-
-  statusBtn.innerHTML = `current weight: ${knapsack.weight}Kg`;
-});
+clearAllBtn.onclick = function reset() {
+  window.location.reload();
+};
 
 clearAllBtn.addEventListener("click", () => {
   window.location.reload(true);
 });
-
-
-
-// program to check pass or fail
