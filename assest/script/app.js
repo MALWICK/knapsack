0532e4e5d7ae6b7ed86f6a3ef989ca28d5678892
MALWICK <batahumphrey66@gamil.com>
2023-06-23 15:11:40 +0100

    solved code

--- assest/script/app.js
+++ assest/script/app.js
@@ -6,16 +6,16 @@ const maxWeight = document.querySelector('#max-weight')
 const clearAllBtn = document.querySelector('.clear-all')
 const Add = document.querySelector('#add')
 
+const itemInput = document.querySelector('#item')
+const weightInput = document.querySelector('#weight')
+const valueInput = document.querySelector('#value')
+const MaximumWeight = document.querySelector('#maxi-weight')
+
 const items = []
 form.addEventListener('submit', addItem)
 
 function addItem (e) {
   e.preventDefault()
-
-  const itemInput = document.querySelector('#item')
-  const weightInput = document.querySelector('#weight')
-  const valueInput = document.querySelector('#value')
-  const MaximumWeight = document.querySelector('#maxi-weight')
 
   const item = {
     name: itemInput.value,
@@ -24,18 +24,9 @@ function addItem (e) {
     maxw: parseInt(MaximumWeight.value)
   }
 
-  if (item.weight > item.maxw) {
-    window.alert(`The maximum weight allowed is ${item.maxw}`)
-    console.log(`The maximum weight allowed is ${item.maxw} and its full`)
-    Add.style.backgroundColor = 'red'
-    Add.disabled = true
-    return
-  }
-
-  console.log(item.maxw, item.weight)
   items.push(item)
 
-  displayItems()
+  calculateResult()
 
   itemInput.value = ''
   weightInput.value = ''
@@ -43,44 +34,21 @@ function addItem (e) {
   MaximumWeight.disabled = true
 }
 
-function displayItems () {
-  itemList.innerHTML = ''
-  items.forEach((item, index) => {
-    if (item.weight < item.maxw) {
-      const li = document.createElement('li')
-      li.innerHTML = `  <span>${item.name}</span>
-      <span>${item.weight}Kg</span>
-      <span>${item.value}XFA</span>
-      <button class="delete" onclick="removeItem(${index})">Remove</button>`
-      itemList.appendChild(li)
-    } else {
-      window.alert(`The maximum weight allowed is ${item.maxw}`)
-      console.log(`The maximum weight allowed is ${item.maxw} and its full`)
-      Add.style.backgroundColor = 'red'
-      Add.disabled = true
-    }
-  })
-
-  calculateResult()
-}
-
-function removeItem (index) {
-  items.splice(index, 1)
-  console.log(index)
-  displayItems()
-}
-
-removeItem()
-
 function calculateResult () {
   let totalWeightValue = 0
   let totalValueValue = 0
   let maxWeigh = 0
+  let itemName
+  let weightInput
+  let valueInput
 
   items.forEach((item) => {
     totalWeightValue += item.weight
     totalValueValue += item.value
     maxWeigh = item.maxw
+    itemName = item.name
+    weightInput = item.weight
+    valueInput = item.value
   })
 
   if (totalWeightValue > maxWeigh) {
@@ -89,6 +57,13 @@ function calculateResult () {
     Add.style.backgroundColor = 'red'
     Add.disabled = true
   } else {
+    const li = document.createElement('li')
+    li.style.backgroundColor = '#afe1af'
+    li.innerHTML = `  <span>${itemName}</span>
+      <span>${weightInput}Kg</span>
+      <span>${valueInput}XFA</span>
+     `
+    itemList.appendChild(li)
     totalWeight.textContent = totalWeightValue
     totalValue.textContent = totalValueValue
     maxWeight.textContent = maxWeigh
