b8f95c62eb47ab319dd348dac5efc206bc9f29b5
MALWICK <batahumphrey66@gamil.com>
2023-07-12 17:01:55 +0100

    added loaded

--- assest/script/app.js
+++ assest/script/app.js
@@ -1,79 +1,92 @@
-const form = document.querySelector('form')
+const items = [
+  { name: 'book', weight: 8, value: 200 },
+  { name: 'pen', weight: 2, value: 100 },
+  { name: 'ruler', weight: 4, value: 300 },
+  { name: 'pencil', weight: 3, value: 100 },
+  { name: 'calculator', weight: 5, value: 1000 },
+  { name: 'sweet', weight: 1, value: 50 },
+  { name: 'biscuit', weight: 3, value: 25 },
+  { name: 'bread', weight: 6, value: 100 },
+  { name: 'doughnut', weight: 7, value: 50 },
+  { name: 'cake', weight: 5, value: 125 },
+  { name: 'shoe', weight: 12, value: 5000 },
+  { name: 'balloon', weight: 5, value: 25 },
+  { name: 'computer', weight: 50, value: 100000 },
+  { name: 'iPhone', weight: 15, value: 300000 },
+  { name: 'stopwatch', weight: 2, value: 800 },
+  { name: 't-shirt', weight: 4, value: 2500 },
+  { name: 'coat', weight: 8, value: 15000 },
+  { name: 'handkerchief', weight: 2, value: 700 },
+  { name: 'USB-key', weight: 3, value: 5000 },
+  { name: 'smartphone', weight: 2, value: 75000 }
+]
+
+const options = document.getElementById('select')
+const maxCapacity = document.querySelector('.capacity')
+const results = document.querySelector('.final-state')
+const done = document.querySelector('.btn1')
+const add = document.querySelector('.btn2')
+const restart = document.querySelector('.btn3')
 const itemList = document.querySelector('#item-list')
-const totalWeight = document.querySelector('#total-weight')
-const totalValue = document.querySelector('#total-value')
-const maxWeight = document.querySelector('#max-weight')
-const clearAllBtn = document.querySelector('.clear-all')
-const Add = document.querySelector('#add')
+let sumWeight = 0
+let sumValue = 0
 
-const itemInput = document.querySelector('#item')
-const weightInput = document.querySelector('#weight')
-const valueInput = document.querySelector('#value')
-const MaximumWeight = document.querySelector('#maxi-weight')
+done.onclick = function getOption () {
+  const inputVal = maxCapacity.value
+  maxCapacity.disabled = true
+  done.disabled = true
 
-const items = []
-form.addEventListener('submit', addItem)
-
-function addItem (e) {
-  e.preventDefault()
-
-  const item = {
-    name: itemInput.value,
-    weight: parseInt(weightInput.value),
-    value: parseInt(valueInput.value),
-    maxw: parseInt(MaximumWeight.value)
+  if (inputVal === '') {
+    window.alert('please enter the max-width of the knapsack')
+    window.location.reload()
   }
 
-  items.push(item)
-
-  calculateResult()
-
-  itemInput.value = ''
-  weightInput.value = ''
-  valueInput.value = ''
-  MaximumWeight.disabled = true
-}
-
-function calculateResult () {
-  let totalWeightValue = 0
-  let totalValueValue = 0
-  let maxWeigh = 0
-  let itemName
-  let weightInput
-  let valueInput
-
-  items.forEach((item) => {
-    totalWeightValue += item.weight
-    totalValueValue += item.value
-    maxWeigh = item.maxw
-    itemName = item.name
-    weightInput = item.weight
-    valueInput = item.value
-  })
-
-  if (totalWeightValue > maxWeigh) {
-    window.alert(`The maximum weight allowed is ${maxWeigh}`)
-    console.log(`The maximum weight allowed is ${maxWeigh} and its full`)
-    Add.style.backgroundColor = 'red'
-    Add.disabled = true
-  } else {
-    const li = document.createElement('li')
-    li.style.backgroundColor = '#afe1af'
-    li.innerHTML = `  <span>${itemName}</span>
-      <span>${weightInput}Kg</span>
-      <span>${valueInput}XFA</span>
+  add.addEventListener('click', () => {
+    for (let i = 0; i < items.length; i++) {
+      if (options.value === items[i].name) {
+        sumWeight += items[i].weight
+        sumValue += items[i].value
+        console.log(sumWeight)
+        console.log(sumValue)
+        if (sumWeight <= inputVal) {
+          itemList.style.backgroundColor = '#e2f4e2'
+          const li = document.createElement('li')
+          li.innerHTML = `
+            <span>${options.value}</span>
+            <span>${items[i].weight}kg</span>
+            <span>${items[i].value}fcfa</span>
      `
-    itemList.appendChild(li)
-    totalWeight.textContent = totalWeightValue
-    totalValue.textContent = totalValueValue
-    maxWeight.textContent = maxWeigh
-  }
+          itemList.appendChild(li)
+
+          const deleteBtn = document.createElement('button')
+          deleteBtn.innerText = 'Delete'
+          deleteBtn.classList.add('delete')
+          li.appendChild(deleteBtn)
+
+          deleteBtn.addEventListener('click', () => {
+            sumWeight -= items[i].weight
+            li.remove()
+          })
+
+          results.innerHTML = `
+            <div>
+              <p>Total weight: ${sumWeight}</p>
+              <p>Total value(XAF): ${sumValue}</p>
+              <p>Max weight: ${inputVal}</p>
+            </div>
+          `
+          add.style.backgroundColor = '#4caf50'
+        } else {
+          window.alert(`Exceeded maximum weight of ${inputVal}kg you can reduce the items in the knapsack `)
+          done.disabled = true
+          itemList.style.backgroundColor = '#f08080'
+          add.style.backgroundColor = '#f08080'
+        }
+      }
+    }
+  })
 }
 
-clearAllBtn.onclick = function reset () {
+restart.onclick = function reset () {
   window.location.reload()
 }
-
-clearAllBtn.addEventListener('click', () => {
-  window.location.reload(true)
-})
